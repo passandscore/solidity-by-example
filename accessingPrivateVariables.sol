9e763dd2f23d684a98355199c099324c8af6349f
Jason Schwarz <jason.c.schwarz@gmail.com>
2022-06-29 04:47:20 -0300

    Create accessingPrivateVariables.sol

---
+++ accessingPrivateVariables.sol
@@ -0,0 +1,5 @@
+// Web3
+const storageItem = await web3.eth.getStorageAt("0x790eA4057B8a394C74973e32d54D3e49D3AE3705","1")
+
+// Ethers
+const storageItem = await provider.getStorageAt("0x790eA4057B8a394C74973e32d54D3e49D3AE3705","1")
