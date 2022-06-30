f3617e79b5c3e1a52f0e5908b5b2a85c5787afc3
Jason Schwarz <jason.c.schwarz@gmail.com>
2022-06-30 16:47:14 -0300

    Update Call.sol

--- Call.sol
+++ Call.sol
@@ -46,3 +46,12 @@ contract Caller {
         emit Response(success, data);
     }
 }
+
+// DECODING RETURN VALUES
+contract SomeContract {
+    function any() public {
+        (bool success, bytes memory result) = addr.call(abi.encodeWithSignature("myFunction(uint,address)", 10, msg.sender);
+        // if success is `true` the function exists and it had returned some result
+        (uint a, uint b) = abi.decode(result, (uint, uint)); // This is an example of how the result might have been decoded
+    }
+}
