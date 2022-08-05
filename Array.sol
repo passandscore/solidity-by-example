ea77db1855d4a1a2811530fd918586bf009ec0be
Jason Schwarz <jason.c.schwarz@gmail.com>
2022-08-05 12:29:44 -0400

    Update Array.sol

--- Array.sol
+++ Array.sol
@@ -49,3 +49,37 @@ contract Array {
         uint[] memory a = new uint[](5);
     }
 }
+
+//-------------------------------
+// Replace values at given index
+//-------------------------------
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract IndexEraser{
+
+    uint[] private arr1 = [0,1,2,3,4,5,6,7,8];
+
+    function removeIndex(uint index)public returns(uint[] memory _arr){
+        require(index < arr1.length -1, "INDEX OUT OF BOUNDS!!");
+        _arr = arr1;
+        _arr[index] = _arr[_arr.length -1];
+        arr1 = _arr;
+        arr1.pop(); 
+    }
+
+    function removeIndexOrdered(uint index)public returns(uint[] memory _arr){
+        require(index < arr1.length -1, "INDEX OUT OF BOUNDS!!");
+        _arr = arr1;
+        for(uint i = index; i < _arr.length - 1; i++){
+            _arr[i] = _arr[i + 1];
+        }
+        arr1 = _arr;
+        arr1.pop();
+    }
+
+    function viewArray()public view returns(uint[] memory){
+        return arr1;
+    }
+}
