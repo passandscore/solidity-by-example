ab75c806b68cff8ced35ba05b3a92488213a9d55
Jason Schwarz <jason.c.schwarz@gmail.com>
2022-10-30 12:56:01 -0300

    Update Immutable.sol

--- Immutable.sol
+++ Immutable.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.10;
 
 // Immutable variables are like constants. Values of immutable variables can be set inside the constructor but cannot be modified afterwards.
+// TypeError: Cannot write to immutable here: Immutable variables can only be initialized inline or assigned directly in the constructor.
 
 contract Immutable {
     // coding convention to uppercase constant variables
